cb8e5ed7dc8b3be2a58fdff118d448ebd8c0f215
ecell-vitc <helloecellvitc@gmail.com>
2025-12-14 18:24:01 +0530

    Added test env CORS

--- server/main.py
+++ server/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://stock-market-ecell.vercel.app"
+        "https://stock-market-ecell.vercel.app",
+        "*.ngrok-free.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
